5a174be942ba89df4d651096474e4a8530b8ef77
dammy995 <damianemerah1@gmail.com>
2023-06-26 18:07:18 +0100

    styled the menu btn

--- index.py
+++ index.py
@@ -1,28 +1,19 @@
-# def quotientstring(x,y):
-#     quotient = x / y
-#     reminder = x % y
-#     # return ("The quotient of {} and {} is {}, and the reminder is {}.".format(x,y,quotient, reminder), sep="")
-#     return ("The quotient of ", x," and ", y, " is ",quotient, " and the reminder is ", reminder)
-
-
-# # def main():
-# #     x = int(input("Enter a number X:__"))
-# #     y = int(input("Enter another number Y:__"))
-
-# # main()
-# print(quotientstring(25, 5))
-
-
-def quotientprob(x, y):
-    quotient = x//y
-    remainder = x % y
-    print('The quotient of ', x, ' and ', y, ' is ', quotient, ' with a remainder of ', remainder, '.', sep='')
+def quotientstring(x,y):
+    quotient = x / y
+    reminder = x % y
+    # return ("The quotient of {} and {} is {}, and the reminder is {}.".format(x,y,quotient, reminder), sep="")
+    return ("The quotient of ", x," and ", y, " is ",quotient, " and the reminder is ", reminder)
 
 
 def main():
-    x = int(input('Enter an number: '))
-    y = int(input('Enter another number: '))
-    quotientprob(x, y)
-    
+    x = int(input("Enter a number X:__"))
+    y = int(input("Enter another number Y:__"))
 
 main()
+
+
+items = ['orange', 'mango', 'cashew', 'guava']
+number = 1
+
+for item in items:
+    print(number + 1, item)
